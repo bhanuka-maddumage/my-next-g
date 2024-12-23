3480729a39ef98620c66765d797bd1ca3187a37c
bhanuka-maddumage <bhanuka.maddumage@smashydesign.com>
2024-12-23 11:26:17 +0530

    Initial commit

---
+++ src/api/resturant-category/routes/resturant-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resturant-category router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::resturant-category.resturant-category');
---
+++ src/api/resturant-category/services/resturant-category.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resturant-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::resturant-category.resturant-category');
